a8d0d0ba1c94a62c1412a2bf6db5de5c2879e1de
Fernando Miguel <miguel@itnora.com>
2023-08-27 00:18:13 -0600

    add naive-ui and icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { NCard, NForm, NFormItem, NInput, NIcon } from 'naive-ui'
+import { DocumentText } from '@vicons/ionicons5'
+const textInput = ref('')
 </script>
 
 <template>
@@ -6,5 +9,15 @@
     <h1 text-red-600>
       Hola
     </h1>
+    <n-icon>
+      <document-text />
+    </n-icon>
+    <n-card embedded>
+      <n-form>
+        <n-form-item label="Introduce the travel information">
+          <n-input v-model:value="textInput" type="text" placeholder="Basic Input" />
+        </n-form-item>
+      </n-form>
+    </n-card>
   </div>
 </template>
